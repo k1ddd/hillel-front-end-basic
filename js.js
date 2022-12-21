f96ed002dccce8520c69596612e6c67c52a8cb87
vladyslav <vladyslav.klokun@gmail.com>
2022-12-21 23:57:21 +0200

    new page add

--- js.js
+++ js.js
@@ -6,3 +6,9 @@ $(".burger").click(function () {
 $("#mySlider").slick({
   dots: true,
 });
+
+$(document).ready(function () {
+  $("#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4").click(function () {
+    $(this).toggleClass("open");
+  });
+});
